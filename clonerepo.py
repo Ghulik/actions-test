218578b7b5534d13520d96692dfbd07f6764465d
Marcel Brimus <Ghulik@users.noreply.github.com>
2021-04-11 10:59:19 +0200

    Update clonerepo.py

--- clonerepo.py
+++ clonerepo.py
@@ -5,7 +5,7 @@ from github import Github
 
 #org:github extension:js test
 
-g = Github("${{ secrets.GITHUB_TOKEN }}")
+g = Github("${{ process.env.API_KEY }}")
 
 results = g.search_code('search_code')
 
