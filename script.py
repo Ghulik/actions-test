459481fa73d8dc46b7c9ff17e879b3b6ec423d19
Marcel Brimus <Ghulik@users.noreply.github.com>
2021-04-11 09:28:31 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
-for root, dirs, files in os.walk("${{ github.workspace }}"):
+github_workspace = os.getenv("GITHUB_WORKSPACE")
+for root, dirs, files in os.walk(github_workspace):
     for file in files:
         print(os.path.join(root, file))
